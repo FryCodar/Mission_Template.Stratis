0d1bdc382aa268933c0d14e554b2284fde6a3ca7
FryCodar <interceptor1980@yahoo.de>
2018-06-04 22:10:30 +0200

    aDD

---
+++ functions/intout/cfgFunctions.hpp
@@ -0,0 +1,14 @@
+#include "msot_components.hpp"
+
+class TAGADD
+{
+	class intout
+	{
+		class playintro {
+					            description = "Play the Intro of MSOT";
+				            };
+    class runcamera {
+                      description = "Camera Script";
+                    };
+	};
+};
