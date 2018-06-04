c6fa923ee40c9600cf4d64c2e8f6d8104869737f
FryCodar <interceptor1980@yahoo.de>
2018-06-04 23:07:19 +0200

    add Outro

--- functions/intout/cfgFunctions.hpp
+++ functions/intout/cfgFunctions.hpp
@@ -7,6 +7,9 @@ class TAGADD
 		class playintro {
 					            description = "Play the Intro of MSOT";
 				            };
+		class playoutro {
+											description = "Play the Outro of MSOT";
+										};
     class runcamera {
                       description = "Camera Script";
                     };
