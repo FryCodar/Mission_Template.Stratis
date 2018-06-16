39af84db4f07abf85983d183931de495769a9d4b
FryCodar <interceptor1980@yahoo.de>
2018-06-16 23:33:58 +0200

    Add new Code

--- functions/dialog/dialog.hpp
+++ functions/dialog/dialog.hpp
@@ -26,13 +26,13 @@ onUnload = "uiNamespace setVariable ['msot_dlg',false];";
     {
 	     idc = 10014;//1200
 	     text = "pics\MSOT.paa";
-	     x = X_CENTERED(0.5);//0.438151 * safezoneW + safezoneX;
+	     x = X_CENTERED(0.46);//0.438151 * safezoneW + safezoneX;
 	     y = Y_CENTERED(0.5);//0.33496 * safezoneH + safezoneY;
-	     w = W_SIZE(0.5);
+	     w = W_SIZE(0.46);
 	     h = H_SIZE(0.5);
     };
 
-    class DIARSC(Close_All,Btn): DIARSC(normal,RscPicButton)
+    class DIARSC(Back_All,Btn): DIARSC(normal,RscPicButton)
 		{
 			idc = 10015;
       colorText[] = {0,0,0,0};
@@ -43,13 +43,83 @@ onUnload = "uiNamespace setVariable ['msot_dlg',false];";
     	colorBackgroundActive[] = {0,0,0,0};
       colorShadow[] = {0,0,0,0};
     	colorBorder[] = {0,0,0,0};
-			x = X_CENTERED(0.18);
-			y = 0.81 * safezoneH + safezoneY;
-			w = W_SIZE(0.18);
-			h = 0.043 * safezoneH;
+			x = X_CENTERED(0.07);
+			y = Y_CALC(0.797)//0.797 * safezoneH + safezoneY;
+			w = W_SIZE(0.07);
+			h = H_SIZE(0.043);
 			text = "";
       toolTip = "Close Tablet";
 			action = "closeDialog 0";
+		};
+
+    class DIARSC(Artillery_1st,Btn): DIARSC(normal,RscPicButton)
+		{
+			idc = 10016;
+      colorFocused[] = {0,0,0,0};   // border color for focused state
+    	colorDisabled[] = {0,0,0,0};   // text color for disabled state
+    	colorBackground[] = {0,0,0,0};
+    	colorBackgroundDisabled[] = {0,0,0,0};
+      colorShadow[] = {0,0,0,0};
+    	colorBorder[] = {0,0,0,0};
+			x = X_LEFT_SIDED(0.17,0.08);
+			y = Y_UP_SIDED(0.2,0.08)
+			w = W_SIZE(0.17);
+			h = H_SIZE(0.2);
+			text = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\artillery_ca.paa";
+      toolTip = "Call Artillery Strike";
+			action = "hint 'Artillery';";
+		};
+    class DIARSC(Airsupply_2nd,Btn): DIARSC(normal,RscPicButton)
+		{
+			idc = 10017;
+      colorFocused[] = {0,0,0,0};   // border color for focused state
+    	colorDisabled[] = {0,0,0,0};   // text color for disabled state
+    	colorBackground[] = {0,0,0,0};
+    	colorBackgroundDisabled[] = {0,0,0,0};
+      colorShadow[] = {0,0,0,0};
+    	colorBorder[] = {0,0,0,0};
+			x = X_RIGHT_SIDED(0,0.08);
+			y = Y_UP_SIDED(0.2,0.08)
+			w = W_SIZE(0.17);
+			h = H_SIZE(0.2);
+			text = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa";
+      toolTip = "Call Air Supplies";
+			action = "hint 'Airsupply';";
+		};
+    class DIARSC(Aircas_3rd,Btn): DIARSC(normal,RscPicButton)
+		{
+			idc = 10018;
+      colorFocused[] = {0,0,0,0};   // border color for focused state
+    	colorDisabled[] = {0,0,0,0};   // text color for disabled state
+    	colorBackground[] = {0,0,0,0};
+    	colorBackgroundDisabled[] = {0,0,0,0};
+      colorShadow[] = {0,0,0,0};
+    	colorBorder[] = {0,0,0,0};
+			x = X_LEFT_SIDED(0.17,0.08);
+			y = Y_DOWN_SIDED(0,0.08)
+			w = W_SIZE(0.17);
+			h = H_SIZE(0.2);
+      text = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\cas_ca.paa";
+			toolTip = "Call Close Air Support (CAS)";
+			action = "hint 'C A S';";
+		};
+    class DIARSC(Satview_4th,Btn): DIARSC(normal,RscPicButton)
+		{
+			idc = 10019;
+      colorFocused[] = {0,0,0,0};   // border color for focused state
+    	colorDisabled[] = {0,0,0,0};   // text color for disabled state
+    	colorBackground[] = {0,0,0,0};
+    	colorBackgroundDisabled[] = {0,0,0,0};
+      colorShadow[] = {0,0,0,0};
+    	colorBorder[] = {0,0,0,0};
+			x = X_RIGHT_SIDED(0,0.08);
+			y = Y_DOWN_SIDED(0,0.08)
+			w = W_SIZE(0.17);
+			h = H_SIZE(0.2);
+			text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayArsenal\gps_ca.paa";
+      toolTip = "Open SatView System";
+			action = "hint 'SatView';";
+
 		};
   };
 };
--- functions/dialog/dlg_define.hpp
+++ functions/dialog/dlg_define.hpp
@@ -12,14 +12,17 @@
 #define X_CENTERED(dar) (X_SCREEN_SIZE * 0.5) * (1 - (dar * 0.75)) + X_ANCHOR
 #define Y_CENTERED(dar) ((Y_SCREEN_SIZE * 0.5) * (1 - dar)) + Y_ANCHOR
 
-#define W_SIZE(dar) (dar * X_SCREEN_SIZE) * 0.75
+#define X_CALC(dar) ((dar * 0.75) * X_SCREEN_SIZE) + X_ANCHOR
+#define Y_CALC(dar) (dar * Y_SCREEN_SIZE) + Y_ANCHOR
+
+#define W_SIZE(dar) ((dar * 0.75) * X_SCREEN_SIZE)
 #define H_SIZE(dar) (dar * Y_SCREEN_SIZE)
 
-#define X_LEFT_SIDED(dar,dar1) (X_SCREEN_SIZE * 0.5) - (dar + dar1) * 0.75
-#define Y_UP_SIDED(dar,dar1) (Y_SCREEN_SIZE * 0.5) - (dar + dar1)
+#define X_LEFT_SIDED(dar,dar1) ((X_SCREEN_SIZE * 0.5) + X_ANCHOR) - (W_SIZE(dar) + dar1)
+#define Y_UP_SIDED(dar,dar1) ((Y_SCREEN_SIZE * 0.5) + Y_ANCHOR) - (H_SIZE(dar) + dar1)
 
-#define X_RIGHT_SIDED(dar,dar1) (X_SCREEN_SIZE * 0.5) + (dar,dar1) * 0.75
-#define Y_DOWN_SIDED(dar,dar1) (Y_SCREEN_SIZE * 0.5) + (dar + dar1)
+#define X_RIGHT_SIDED(dar,dar1) ((X_SCREEN_SIZE * 0.5) + X_ANCHOR) + (W_SIZE(dar) + dar1)
+#define Y_DOWN_SIDED(dar,dar1) ((Y_SCREEN_SIZE * 0.5) + Y_ANCHOR) + (H_SIZE(dar) + dar1)
 
 /*
 // ******************************************************************************************
